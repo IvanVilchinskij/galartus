d51e0c537c72571708cd702ef06ceac60b1f3aa9
Ivan <ivanvilchinskii20@gmail.com>
2021-05-21 12:38:20 +0300

    fix token_refresh by time

--- src/axios.js
+++ src/axios.js
@@ -78,6 +78,13 @@ axiosInstance.interceptors.response.use(
 						})
 						.catch((err) => {
 							console.log(err);
+                            localStorage.removeItem('access_token');
+                            localStorage.removeItem('refresh_token');
+        
+                            axiosInstance.defaults.headers['Authorization'] = null;
+                            
+                            window.location.href = '/';
+                            window.location.reload();
 						});
 				} else {
 					console.log('Refresh token is expired', tokenParts.exp, now);
